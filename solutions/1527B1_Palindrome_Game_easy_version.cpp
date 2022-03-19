dd07d9736fcc2922b71d22132661583e530ab492
rheallyc <vireonxi@gmail.com>
2022-03-19 07:21:14 +0000

    Solve 1527B1: Palindrome Game (easy version)

---
+++ solutions/1527B1_Palindrome_Game_easy_version.cpp
@@ -0,0 +1,25 @@
+// Problem: Palindrome Game (easy version)
+// URL: https://codeforces.com/problemset/problem/1527/B1
+// Rating: 1200
+// Tags: constructive algorithms, games
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+char s[1100];
+
+void solve(){
+    scanf("%d%s", &n, s+1);
+    int cnt=0;
+    for(int i=1; i<=n; i++) if(s[i]=='0') cnt++;
+    if(cnt%2==0 || cnt==1) printf("BOB\n");
+    else printf("ALICE\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

1e006b8ba59b3e25c57d35c42e4ebc2fba243314
rheallyc <vireonxi@gmail.com>
2022-03-27 20:16:03 +0000

    Solve 1509A: Average Height

---
+++ solutions/1509A_Average_Height.cpp
@@ -0,0 +1,32 @@
+// Problem: Average Height
+// URL: https://codeforces.com/problemset/problem/1509/A
+// Rating: 800
+// Tags: constructive algorithms
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, num;
+vector<int> t1, t2;
+
+void solve(){
+    scanf("%d", &n);
+    t1.clear(); t2.clear();
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+        if(num%2) t1.push_back(num);
+        else t2.push_back(num);
+    }
+    for(auto x:t1) printf("%d ",x);
+    for(auto x:t2) printf("%d ",x);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+

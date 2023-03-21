4c88a1820d34b6297ff78107821e77a9b5bcb0b3
rheallyc <vireonxi@gmail.com>
2023-03-21 04:24:29 +0000

    Solve 1484A: Prison Break

---
+++ solutions/1484A_Prison_Break.cpp
@@ -0,0 +1,21 @@
+// Problem: Prison Break
+// URL: https://codeforces.com/problemset/problem/1484/A
+// Rating: 800
+// Tags: constructive algorithms
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int a, b;
+
+void solve(){
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a*b);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

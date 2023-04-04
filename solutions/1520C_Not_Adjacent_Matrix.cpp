7c10a77614970f125ced453e05e3ec3bc9e59920
rheallyc <vireonxi@gmail.com>
2023-04-04 13:59:10 +0000

    Solve 1520C: Not Adjacent Matrix

---
+++ solutions/1520C_Not_Adjacent_Matrix.cpp
@@ -0,0 +1,43 @@
+// Problem: Not Adjacent Matrix
+// URL: https://codeforces.com/problemset/problem/1520/C
+// Rating: 1000
+// Tags: constructive algorithms
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[110][110];
+
+void solve(){
+    scanf("%d", &n);
+    if(n==1){ printf("1\n"); return;}
+    if(n==2){ printf("-1\n"); return;}
+    int cnt=1;
+    for(int t=n-1; t>=1-n; t-=2){
+        for(int i=max(1, 1+t); i<=min(n+t, n); i++){
+            int j=i-t;
+            a[i][j]=cnt++;
+        }
+    }
+    for(int t=n-2; t>=2-n; t-=2){
+        for(int i=max(1, 1+t); i<=min(n+t, n); i++){
+            int j=i-t;
+            a[i][j]=cnt++;
+        }
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+

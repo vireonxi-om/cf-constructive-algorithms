112fe3d959f1ba3cf0c712a00de3725f27fb43e2
rheallyc <vireonxi@gmail.com>
2023-06-29 13:40:41 +0000

    Solve 1541A: Pretty Permutations

---
+++ solutions/1541A_Pretty_Permutations.cpp
@@ -0,0 +1,33 @@
+// Problem: Pretty Permutations
+// URL: https://codeforces.com/problemset/problem/1541/A
+// Rating: 800
+// Tags: constructive algorithms, greedy, implementation
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    if(n%2){
+        printf("3 1 2 ");
+        for(int i=4; i<n; i+=2){
+            printf("%d %d ", i+1, i);
+        }
+        printf("\n");
+    }
+    else{
+        for(int i=1; i<n; i+=2){
+            printf("%d %d ", i+1, i);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

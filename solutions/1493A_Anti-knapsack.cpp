b03c72c4c607ceea1fd831012f3a701766237433
rheallyc <vireonxi@gmail.com>
2023-11-03 00:19:31 +0000

    Solve 1493A: Anti-knapsack

---
+++ solutions/1493A_Anti-knapsack.cpp
@@ -0,0 +1,28 @@
+// Problem: Anti-knapsack
+// URL: https://codeforces.com/problemset/problem/1493/A
+// Rating: 800
+// Tags: constructive algorithms, greedy
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, k;
+
+void solve(){
+    scanf("%d%d", &n, &k);
+    vector<int> ans;
+    for(int i=n; i>k; i--){
+        ans.push_back(i);
+    }
+    for(int i=k-1; i>=(k+1)/2; i--) ans.push_back(i);
+    printf("%d\n", ans.size());
+    for(auto x: ans) printf("%d ", x);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}

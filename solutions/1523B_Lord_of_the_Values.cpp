af37796fcb793adea98094f6d82ed2aca850d3cd
rheallyc <vireonxi@gmail.com>
2024-10-31 01:11:38 +0000

    Solve 1523B: Lord of the Values

---
+++ solutions/1523B_Lord_of_the_Values.cpp
@@ -0,0 +1,31 @@
+// Problem: Lord of the Values
+// URL: https://codeforces.com/problemset/problem/1523/B
+// Rating: 1100
+// Tags: constructive algorithms
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    int num;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+    }
+    printf("%d\n", 3*n);
+    for(int i=1; i<=n; i+=2){
+        for(int k=1; k<=3; k++){
+            printf("%d %d %d\n", 1, i, i+1);
+            printf("%d %d %d\n", 2, i, i+1);
+        }
+    }
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
